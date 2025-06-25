2790570894825db8215b221a775cc9bd79749304
c8villanueva <caitlin.villanueva@gmail.com>
2025-06-25 16:47:51 +0800

    added home and profile tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import { Tabs } from 'expo-router'
+import React from 'react'
+
+const _layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen 
+        name="index"
+        options={{
+          title: 'Home',
+          headerShown: false
+        }}
+      />
+      <Tabs.Screen 
+        name="profile"
+        options={{
+          title: 'Profile',
+          headerShown: false
+        }}
+      />
+    </Tabs>
+  )
+}
+
+export default _layout
